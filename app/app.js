20cdac1067dbb45ce6a709eceaf56a9ad5fbe707
Miguel Angulo <maangulo12@gmail.com>
2016-02-10 01:07:41 -0600

    Update app

--- app/app.js
+++ app/app.js
@@ -1,11 +1,35 @@
-(function() {
-    'use strict';
+'use strict';
 
-    // Set current time
-    var time = document.querySelector('.time-display');
-    time.innerHTML = moment().format('LT');
+var time    = document.querySelector('.time-display');
+var alarm   = document.querySelector('.alarm-time');
+var mode    = document.querySelector('.hour-checkbox');
+var seconds = document.querySelector('.seconds-checkbox');
 
-    // Set alarm time
-    var alarm = document.querySelector('.alarm-time');
-    alarm.innerHTML = moment().format('LT');
-})();
+// init
+seconds.checked = true;
+updateTime();
+
+// updateTime function
+function updateTime() {
+    var modeChecked    = mode.checked;
+    var secondsChecked = seconds.checked;
+
+    if (modeChecked && secondsChecked) {
+        time.innerHTML  = moment().format('H:mm:ss');
+        alarm.innerHTML = moment().format('H:mm');
+    }
+    else if (modeChecked && !secondsChecked) {
+        time.innerHTML  = moment().format('H:mm');
+        alarm.innerHTML = moment().format('H:mm');
+    }
+    else if (!modeChecked && secondsChecked) {
+        time.innerHTML  = moment().format('h:mm:ss A');
+        alarm.innerHTML = moment().format('h:mm A');
+    }
+    else {
+        time.innerHTML  = moment().format('h:mm A');
+        alarm.innerHTML = moment().format('h:mm A');
+    }
+
+    setTimeout(updateTime, 1000);
+}
